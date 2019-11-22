016b839c42e2a59e8956f228400636de8d835f52
harshisirohi <42697042+harshisirohi@users.noreply.github.com>
2019-11-22 17:35:10 +0530

    Add files via upload

---
+++ SimpleServlet.java
@@ -0,0 +1,29 @@
+package com.capgemini.harshi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "A Simple Servlet Project", urlPatterns = { "/AdvancedServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		System.out.println("Welcome to Simple Servlet Project ");
+		PrintWriter writer =response.getWriter();
+		writer.println("Hii Motu");
+	}
+
+}
